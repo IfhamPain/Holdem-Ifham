8f52a910fe0ffcf25a8a7a8ab251c3096d8e4cf1
Ifham Pain <Ifham Pain>
2017-05-20 18:55:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chavin/holdem/AlarmActivity.java
@@ -0,0 +1,139 @@
+package com.example.chavin.holdem;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
+public class AlarmActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //to make the alarm manager
+    AlarmManager alarm_manager;
+    TimePicker alarm_timepicker;
+    Context context;
+    PendingIntent pending_intent;
+    TextView status;
+    protected int hour;
+    protected int minute;
+    //defining firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm);
+        this.context = this;
+
+        //initializing the alarm manager
+        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        //inititalizing the time picker
+        alarm_timepicker = (TimePicker)findViewById(R.id.timePicker);
+
+        //initialising the status
+        status = (TextView)findViewById(R.id.status);
+
+        //create an instance of a calander
+        final Calendar calendar = Calendar.getInstance();
+
+        //initialize start button
+        Button alarm_on = (Button)findViewById(R.id.buttonNewAlarm);
+
+        final Intent my_intent = new Intent(this.context, Alarm_receiver.class);
+
+
+        //create an onclick listner
+        alarm_on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //setting hours and minutes
+                calendar.set(Calendar.HOUR_OF_DAY, alarm_timepicker.getCurrentHour());
+                calendar.set(Calendar.MINUTE, alarm_timepicker.getCurrentMinute());
+
+                UserModel um = new UserModel();
+                //get the int values of hour and minute
+                hour = (alarm_timepicker.getCurrentHour());
+                minute = (alarm_timepicker.getCurrentMinute());
+                String hour_string = "hh";
+                String minute_string = "mm";
+                //convert the int values to string
+                if (hour < 10 && hour >= 0) {
+                     hour_string = "0" + String.valueOf(hour);
+                }
+                else {
+                     hour_string = String.valueOf(hour);
+                }
+                if(minute<10 && minute >= 0){
+                     minute_string = "0" + String.valueOf(minute);
+                }
+                else {
+                     minute_string = String.valueOf(minute);
+                }
+
+
+
+                //method that changes the status Textbox
+                set_alarm_text("Alarm set to:" + hour_string + ":" + minute_string);
+
+                //create a pending intent to delay the intent until the specified calender time
+                pending_intent = PendingIntent.getBroadcast(AlarmActivity.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+                //set the alarm manager
+                alarm_manager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pending_intent);
+
+                //set alarm variable
+                um.setAlarm(hour_string + ":" + minute_string);
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+                databaseReference.child("Alarm").push().setValue(um);
+
+
+            }
+        });
+
+        //initialize end button
+        Button alarm_off = (Button)findViewById(R.id.buttonStopAlarm);
+        //create an onclick listner
+        alarm_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                //method that changes the status textbox
+                set_alarm_text("Alarm off");
+
+                //cancel the alarm
+                alarm_manager.cancel(pending_intent);
+            }
+        });
+
+    }
+
+    private void set_alarm_text(String output) {
+        status.setText(output);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+}
